a9c9f9489e97cc927ff828bda01e46e59b1735c6
JoseEduardoGR <gonzalezrosalesjoseeduardo@gmail.com>
2025-02-02 16:16:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def clean_downloads():
             print(f"Error al eliminar {file_path}: {e}")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
