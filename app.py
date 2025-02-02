1e29f9abd1bb52c61d1309eb8a54a20b4d04fd7a
JoseEduardoGR <gonzalezrosalesjoseeduardo@gmail.com>
2025-02-02 16:26:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def zip_files(files):
             zipf.write(file, os.path.basename(file))  # Agregar al ZIP sin la ruta completa
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
